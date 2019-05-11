75f445fcb205f5e915130703cc36afa4ace65b35
Archie Godfrey <archiegod@gmail.com>
2019-05-11 15:40:54 +0100

    Changed movement functionality

--- paddle.py
+++ paddle.py
@@ -13,32 +13,19 @@ class Paddle(Sprite):
         return str(" " * self.width)
 
     def setHeight(self, height):
-        if height !== self.height:
+        if height != self.height:
             self.height = height
 
-    def move(self, dir):
-        if 'd' in dir or 'u' in dir:
-            if (self.getYPos() > int(self.height / 2)) and not 'u' in dir:
-                self.dir = dir
-                self.performMove(dir)
-            if (self.getYPos() < self.parentHeight - self.height) and not 'd' in dir:
-                self.dir = dir
-                self.performMove(dir)
-	elif len(dir) > 1 and self.getYPos() - dir != 0:
-		for i in range(self.getYPos(), int(dir[1:])):
-			self.performMove(dir)
-		if self.getYPos() - dir > 0:
-			self.dir = 'u'
-		else:
-			self.dir = 'd'
+    def move(self, pos):
+        self.setYPos(int(pos))
 
     def render(self, dir = None, x = None, y = None):
         if (x != None and y != None):
             self.printToConsole(self.getColor(), x, y, self, self.parentWidth, self.parentHeight)
             return
-        for i in range(0, self.getHeight()):
-            self.printToConsole(Reset, self.getXPos(), self.getYPos() + i, self, self.parentWidth, self.parentHeight)
-        if dir != None:
-            self.move(dir)
-        for i in range(0, self.getHeight()):
-            self.printToConsole(self.getColor(), self.getXPos(), self.getYPos() + i, self, self.parentWidth, self.parentHeight)
+	if dir != None and not 's' in dir and int(dir) != self.getYPos():
+		for i in range(0, self.getHeight()):
+		    self.printToConsole(Reset, self.getXPos(), self.getYPos() + i, self, self.parentWidth, self.parentHeight)
+		self.move(dir)
+		for i in range(0, self.getHeight()):
+		    self.printToConsole(self.getColor(), self.getXPos(), self.getYPos() + i, self, self.parentWidth, self.parentHeight)
